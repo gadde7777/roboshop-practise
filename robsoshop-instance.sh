bf49081975d69540abe5b45b230f2837888934c1
an ga <rao2k42@gmail.com>
2026-02-01 14:11:48 -0500

    test

--- robsoshop-instance.sh
+++ robsoshop-instance.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0d48b1166075f71bf"
+ZONE_ID='Z08598391AA5TMYW8RQL3'
+DOMAIN_NAME='daws88straining.online'
 
 for instance in $@
 
@@ -22,6 +24,8 @@ INSTANCE_ID=$(aws ec2 run-instances \
      --query 'Reservations[].Instances[].PublicIpAddress' \
      --output text
    )
+
+   RECORD_NAME=$DOMAIN_NAME
   else
   IP=$(
       aws ec2 describe-instances \
@@ -29,7 +33,35 @@ INSTANCE_ID=$(aws ec2 run-instances \
       --query 'Reservations[].Instances[].PrivateIpAddress' \
      --output text
   )
+ RECORD_NAME=$instance.$DOMAIN_NAME
   fi
 
   echo "IP Address : $IP"
+
+  aws route53 change-resource-record-sets \
+   --hosted-zone-id $ZONE_ID \
+   --change-batch '
+    {
+    
+    "Comment": "Update A record ",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+        }
+    
+    '
+ echo "record Updated for $instance"
+
 done
